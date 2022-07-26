9c35c53ea8aa68555b58cb441c52fc88c7f3cae6
Nishant <nishant@warehousenow.in>
2022-07-26 10:36:45 +0530

    [nishant] added base project template

---
+++ pages/authenticate.jsx
@@ -0,0 +1,9 @@
+const Authenticate = () => {
+  return (
+    <div>
+      <h1>Authenticate</h1>
+    </div>
+  );
+};
+
+export default Authenticate;
---
+++ pages/index.jsx
@@ -0,0 +1,9 @@
+const Index = () => {
+  return (
+    <div>
+      <h1>Introduction or start page</h1>
+    </div>
+  );
+};
+
+export default Index;
